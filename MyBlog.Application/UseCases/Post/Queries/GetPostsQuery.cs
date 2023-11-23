aff6b86901466e707c742b91d42dbd9d9c448862
berdikulov-571 <mygithub571@gmail.com>
2023-11-23 14:43:10 +0500

    GetPostQuery

---
+++ MyBlog.Application/UseCases/Post/Queries/GetPostsQuery.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MyBlog.Application.Abstration;
+using MyBlog.Application.UseCases.Post.Models;
+
+namespace MyBlog.Application.UseCases.Post.Queries
+{
+    public class GetPostsQuery : IRequest<List<PostDto>>
+    {
+
+    }
+
+    public class GetPostsQueryHandler : IRequestHandler<GetPostsQuery, List<PostDto>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetPostsQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<PostDto>> Handle(GetPostsQuery request, CancellationToken cancellationToken)
+        {
+            var posts = await    _context.Posts.Select(x => new PostDto()
+            {
+                PostId = x.PostId,
+                Content = x.Content,
+                Title = x.Title,
+            }).ToListAsync(cancellationToken);
+
+            return posts;
+        }
+    }
+}
