4660581c14d4421a5c6334dd5ee230898c895d17
berdikulov-571 <mygithub571@gmail.com>
2023-11-23 14:43:27 +0500

    DependesyInjection

---
+++ MyBlog.Application/DependecyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MyBlog.Application
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection service)
+        {
+            service.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return service;
+        }
+    }
+}
