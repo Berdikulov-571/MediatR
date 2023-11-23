f6b58dbb91c74fc602d510ac46aed0ac312d6d1c
berdikulov-571 <mygithub571@gmail.com>
2023-11-23 14:42:48 +0500

    IDbContext

---
+++ MyBlog.Application/Abstration/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Domain.Entities;
+
+namespace MyBlog.Application.Abstration
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Post> Posts { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
