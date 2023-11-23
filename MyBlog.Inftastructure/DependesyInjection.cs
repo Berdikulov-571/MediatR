0db8972ff904929d32d5d0a355c47a9b80ea7d81
berdikulov-571 <mygithub571@gmail.com>
2023-11-23 14:44:22 +0500

    DependesyInjection

---
+++ MyBlog.Inftastructure/DependesyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyBlog.Application.Abstration;
+using MyBlog.Inftastructure.Persistance;
+
+namespace MyBlog.Inftastructure
+{
+    public static class DependesyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
