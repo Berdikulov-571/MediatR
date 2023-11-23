61152490cbd479459c922c4543eac7244e9b8830
berdikulov-571 <mygithub571@gmail.com>
2023-11-23 14:43:04 +0500

    PostDto

---
+++ MyBlog.Application/UseCases/Post/Models/PostDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBlog.Application.UseCases.Post.Models
+{
+    public class PostDto
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
