a99fcb30cd5d70f459d736d9008a338d2ff9f1e6
berdikulov-571 <mygithub571@gmail.com>
2023-11-23 14:44:06 +0500

    PostEntityTypeConfigurations

---
+++ MyBlog.Inftastructure/Persistance/EntityTypeConfiguration/PostEntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyBlog.Domain.Entities;
+
+namespace MyBlog.Inftastructure.Persistance.EntityTypeConfiguration
+{
+    public class PostEntityTypeConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.HasKey(x => x.PostId);
+
+            builder.Property(x => x.Title)
+                .HasMaxLength(20)
+                .IsRequired();
+        }
+    }
+}
