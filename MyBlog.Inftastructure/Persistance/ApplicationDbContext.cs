d77fd793b81a48aab553ba4f507d49e45b659d94
berdikulov-571 <mygithub571@gmail.com>
2023-11-23 14:44:13 +0500

    ApplicationDbContext

---
+++ MyBlog.Inftastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Application.Abstration;
+using MyBlog.Domain.Entities;
+using MyBlog.Inftastructure.Persistance.EntityTypeConfiguration;
+
+namespace MyBlog.Inftastructure.Persistance
+{
+    public class ApplicationDbContext : DbContext , IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new PostEntityTypeConfiguration());
+        }
+    }
+}
